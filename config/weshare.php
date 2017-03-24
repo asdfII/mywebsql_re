3a666543c2d29be4a21db085903a93aa7088068e
asdfii <asdfii@126.com>
2017-03-24 11:00:04 +0800

    Add dblist.conf to set DBServers List

--- config/weshare.php
+++ config/weshare.php
@@ -1,20 +1,13 @@
 <?php
-$SERVER_LIST = array(
-                    'Localhost MySQL' => array(
-                                              'host'     => 'localhost',
-                                              'driver'   => extension_loaded('mysqli') ? 'mysqli' : 'mysql5'
-                                         ),
-                    '192.168.163.100' => array(
-                                              'host'     => '192.168.163.100',
-                                              'driver'   => extension_loaded('mysqli') ? 'mysqli' : 'mysql5'
-                                         ),
-                    'Remotehost01'    => array(
-                                              'host'     => 'Remote IP01',
-                                              'driver'   => extension_loaded('mysqli') ? 'mysqli' : 'mysql5'
-                                         ),
-                    'Remotehost02'    => array(
-                                              'host'     => 'Remote IP02',
-                                              'driver'   => extension_loaded('mysqli') ? 'mysqli' : 'mysql5'
-                                         ),
-               );
+$SERVER_LIST = array();
+$file = BASE_PATH . "/dblist.conf";
+$fp = fopen($file, 'rb');
+while ( ($line = fgets($fp)) !== false) {
+        $line = str_replace(array("\r\n", "\r", "\n"), "", $line);
+        $SERVER_LIST[$line] = array(
+                                   'host'     => "$line",
+                                   'driver'   => extension_loaded('mysqli') ? 'mysqli' : 'mysql5'
+                                   );
+}
+fclose($fp);
 ?>
